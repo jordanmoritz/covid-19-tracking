29bab5e540334a55b7a7dc6585921a62a838135d
unknown <moritzjordan@gmail.com>
2020-07-15 16:18:01 -0700

    Union loop for each date column in unpivot

--- dbt_covid_tracking/models/covid_cases_column_test.sql
+++ dbt_covid_tracking/models/covid_cases_column_test.sql
@@ -1,8 +1,21 @@
-{% set columns_to_unpivot %}
-    {{ columns_to_list(
-          'dbt_dev_jm',
-          'county_cases_date_columns',
-          ignore=['state', 'county_name']) }}
-{% endset %}
+{%- set columns_to_unpivot = columns_to_list(
+    'dbt_dev_jm',
+    'county_cases_date_columns',
+    ignore=['state', 'county_name']) -%}
 
-select 1 as field
+{% for column in columns_to_unpivot -%}
+select
+    state,
+    county_name,
+    parse_date('%m/%d/%y'
+              , '{{ column.split('_')[1] }}/{{ column.split('_')[2] }}/{{ column.split('_')[3] }}')
+              as date,
+    {{ column }} as cases
+from
+    {{ source('dbt_dev_jm', 'county_cases_date_columns') }}
+
+{%- if not loop.last %}
+union all
+{% endif %}
+
+{%- endfor %}
