4b6adab9622095b9f03bf008817b87bf1c8f9dca
unknown <moritzjordan@gmail.com>
2020-07-16 16:51:52 -0700

    Added cleansed model for cases. Deleted test and example models

--- dbt_covid_tracking/models/cleansed/us_state_county_daily_cases.sql
+++ dbt_covid_tracking/models/cleansed/us_state_county_daily_cases.sql
@@ -1,18 +1,20 @@
 {%- set columns_to_unpivot = columns_to_list(
-    'dbt_dev_jm',
-    'county_cases_date_columns',
-    ignore=['state', 'county_name']) -%}
+    'covid_sources',
+    'usafacts_confirmed_cases',
+    ignore=['state', 'state_fips_code', 'county_name', 'county_fips_code']) -%}
 
 {% for column in columns_to_unpivot -%}
 select
+    state_fips_code,
     state,
+    county_fips_code,
     county_name,
     parse_date('%m/%d/%y',
               '{{ column.split('_')[1] }}/{{ column.split('_')[2] }}/{{ column.split('_')[3] }}')
               as date,
     {{ column }} as cases
 from
-    {{ source('dbt_dev_jm', 'county_cases_date_columns') }}
+    {{ source('covid_sources', 'usafacts_confirmed_cases') }}
 
 {%- if not loop.last %}
 union all
