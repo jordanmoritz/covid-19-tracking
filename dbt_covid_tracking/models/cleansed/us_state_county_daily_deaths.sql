2eeb8df1e4321c0fd892034f340518287fff1cae
unknown <moritzjordan@gmail.com>
2020-07-16 17:01:43 -0700

    Added daily death model

---
+++ dbt_covid_tracking/models/cleansed/us_state_county_daily_deaths.sql
@@ -0,0 +1,23 @@
+{%- set columns_to_unpivot = columns_to_list(
+    'covid_sources',
+    'usafacts_deaths',
+    ignore=['state', 'state_fips_code', 'county_name', 'county_fips_code']) -%}
+
+{% for column in columns_to_unpivot -%}
+select
+    state_fips_code,
+    state,
+    county_fips_code,
+    county_name,
+    parse_date('%m/%d/%y',
+              '{{ column.split('_')[1] }}/{{ column.split('_')[2] }}/{{ column.split('_')[3] }}')
+              as date,
+    {{ column }} as deaths
+from
+    {{ source('covid_sources', 'usafacts_deaths') }}
+
+{%- if not loop.last %}
+union all
+{% endif %}
+
+{%- endfor %}
