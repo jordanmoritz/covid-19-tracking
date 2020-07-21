b03d7887b1fb520d2682a3d556b7d904f589a0c5
unknown <moritzjordan@gmail.com>
2020-07-21 14:11:56 -0700

    Updated daily deaths model to use macros for incremental

--- dbt_covid_tracking/models/cleansed/us_state_county_daily_deaths.sql
+++ dbt_covid_tracking/models/cleansed/us_state_county_daily_deaths.sql
@@ -5,30 +5,68 @@
   )
 }}
 
+{%- set max_partition_date = max_date_value(
+    'big-query-horse-play',
+    'dbt_covid_dev_cleansed',
+    'us_state_county_daily_deaths',
+    'date') -%}
+
 {%- set columns_to_unpivot = columns_to_list(
     'big-query-horse-play',
     'covid_sources',
     'usafacts_deaths',
     ignore=['state', 'state_fips_code', 'county_name', 'county_fips_code']) -%}
 
-{% for column in columns_to_unpivot -%}
-select
-    -- Some states have malformed codes for whatever reason
-    if(length(state_fips_code) = 3
-      , substr(state_fips_code, 2)
-      , state_fips_code) as state_fips_code,
-    state as state_abbreviation,
-    county_fips_code,
-    county_name,
-    parse_date('%m/%d/%y',
-              '{{ column.split('_')[1] }}/{{ column.split('_')[2] }}/{{ column.split('_')[3] }}')
-              as date,
-    {{ column }} as cumulative_deaths
-from
-    {{ source('covid_sources', 'usafacts_deaths') }}
+{# In case where cleansed table is not currently built #}
+{%- if max_partition_date is none -%}
 
-{%- if not loop.last %}
-union all
-{% endif %}
+    {# Then construct this model using entire column list from source table #}
+    {% for column in columns_to_unpivot -%}
+    select
+        -- Some states have malformed codes for whatever reason
+        if(length(state_fips_code) = 3
+          , substr(state_fips_code, 2)
+          , state_fips_code) as state_fips_code,
+        state as state_abbreviation,
+        county_fips_code,
+        county_name,
 
-{%- endfor %}
+        {%- set date_parts = column.split('_') %}
+        parse_date('%m/%d/%y',
+                  '{{ date_parts[1] }}/{{ date_parts[2] }}/{{ date_parts[3] }}')
+                  as date,
+        {{ column }} as cumulative_deaths
+    from
+        {{ source('covid_sources', 'usafacts_deaths') }}
+
+    {%- if not loop.last %}
+    union all
+    {% endif %}
+
+    {%- endfor %}
+
+{# Else use the max date in cleansed table to determine which fields to unpivot
+so we dont rebuild the entire table for no reason #}
+{%- else -%}
+
+    {%- set dates_to_unpivot = get_dates_to_unpivot(columns_to_unpivot, max_partition_date) -%}
+
+    {% for date in dates_to_unpivot -%}
+    select
+        state_fips_code,
+        state as state_abbreviation,
+        county_fips_code,
+        county_name,
+        date '{{ date }}' as date,
+        {# Have to reconstruct date column name from date value #}
+        {{ format_date_column(date) }} as cumulative_deaths
+    from
+        {{ source('covid_sources', 'usafacts_deaths') }}
+
+    {%- if not loop.last %}
+    union all
+    {% endif %}
+
+    {%- endfor %}
+
+{%- endif -%}
