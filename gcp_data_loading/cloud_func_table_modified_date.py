833c576b85ae97429e843b9275a4fa84cd0a01e6
unknown <moritzjordan@gmail.com>
2020-07-22 17:51:52 -0700

    Working on cloud func for updating table data

--- gcp_data_loading/cloud_func_table_modified_date.py
+++ gcp_data_loading/cloud_func_table_modified_date.py
@@ -1,38 +1,38 @@
 from google.cloud import bigquery
 
-client = bigquery.Client()
+destination_client = bigquery.Client()
 public_client = bigquery.Client(project='bigquery-public-data')
 
-usafacts_dataset_id = 'covid19_usafacts'
+# For destination tables
 destination_dataset_id = 'covid_sources'
-
-usafacts_dataset = public_client.get_dataset(usafacts_dataset_id)
-destination_dataset = client.get_dataset(destination_dataset_id)
-
-
-
-usafacts_tables_to_check = ['confirmed_cases', 'deaths']
 destination_tables_to_check = ['usafacts_confirmed_cases', 'usafacts_deaths']
 
-def get_tables_from_dataset(dataset, tables, client):
+# For source tables
+source_dataset_id = 'covid19_usafacts'
+source_tables_to_check = ['confirmed_cases', 'deaths']
+
+def get_tables_from_dataset(dataset_id, table_names, client):
+    """
+    Uses dataset id, list of table names to get BQ table object
+    """
+    dataset = client.get_dataset(dataset_id)
     table_list = []
-    for table in tables:
+    for table in table_names:
         table_ref = dataset.table(table)
         table_list.append(client.get_table(table_ref))
 
     return table_list
 
-usafacts_tables = get_tables_from_dataset(usafacts_dataset, usafacts_tables_to_check, public_client)
-
-print('usafacts tables:', usafacts_tables)
-
-for table in usafacts_tables:
-    print('modified', table.modified, sep=':')
-
 destination_tables = get_tables_from_dataset(destination_dataset, destination_tables_to_check, client)
+source_tables = get_tables_from_dataset(usafacts_dataset, usafacts_tables_to_check, public_client)
 
-print('dest tables:', destination_tables)
+def compare_table_dates(first_table, second_table):
+    if first_table > second_table:
+        return True
 
+
+
+for table
 test_fact_date = usafacts_tables[0].modified
 test_dest_date = destination_tables[0].modified
 
