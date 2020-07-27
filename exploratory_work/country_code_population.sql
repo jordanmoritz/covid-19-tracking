dc96d0110d328f38969d1a48e6c54d846555e8e3
unknown <moritzjordan@gmail.com>
2020-07-27 13:54:29 -0700

    Merge master into branch

---
+++ exploratory_work/country_code_population.sql
@@ -0,0 +1,21 @@
+SELECT
+  DISTINCT a.countries_and_territories AS source_country_value,
+  b.country AS mapping_country,
+  b.country_code AS mapping_country_code,
+  a.geo_id AS source_geo_id,
+  a.pop_data_2019,
+  b.year_2018,
+  a.pop_data_2019 - b.year_2018 AS pop_diff --we want pop data from ecdc (commonly eurostat it seems?)
+FROM
+  `bigquery-public-data.covid19_ecdc.covid_19_geographic_distribution_worldwide` AS a
+LEFT JOIN
+  `bigquery-public-data.world_bank_global_population.population_by_country` AS b
+ON
+  b.country_code = a.country_territory_code
+-- WHERE
+-- I can factor in for this
+--   b.country IS NULL
+ORDER BY
+--   a.countries_and_territories ASC
+--   pop_diff DESC
+  a.pop_data_2019 DESC
