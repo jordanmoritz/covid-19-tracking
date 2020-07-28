0e6613bef7f13d2abb4401e8ad2247bc0ab6a7d7
unknown <moritzjordan@gmail.com>
2020-07-27 17:56:36 -0700

    Added mapping for country names. Updated country model

--- dbt_covid_tracking/models/cleansed/ecdc_country_daily_volume.sql
+++ dbt_covid_tracking/models/cleansed/ecdc_country_daily_volume.sql
@@ -17,17 +17,36 @@ expontentially more expensive than providing the string value as var
     'date') -%}
 
 select
-    date,
-    geo_id,
-    country_territory_code,
-    countries_and_territories,
-    pop_data_2019 as country_population,
-    daily_confirmed_cases as new_cases,
-    daily_deaths as new_deaths,
-    confirmed_cases as cumulative_cases,
-    deaths as cumulative_deaths,
+    country_data.date,
+    country_data.geo_id,
+    country_data.country_territory_code,
+
+    case
+        when country_map.country is not null then country_map.country
+        -- Plug for values lacking matched codes
+        when country_data.country_territory_code in ('AIA', 'BES', 'GGY', 'JEY', 'MSF', 'CNG1925')
+            then country_data.countries_and_territories
+        when country_data.country_territory_code = 'FLK' then 'Falkland Islands'
+        when country_data.country_territory_code = 'VAT' then 'Holy See'
+        when country_data.country_territory_code = 'ESH' then 'Western Sahara'
+        when country_data.countries_and_territories = 'Cases_on_an_international_conveyance_Japan'
+            then 'Japan International Conveyance'
+        else 'Undefined'
+        end
+        as country_territory_name,
+
+    country_data.pop_data_2019 as country_territory_population,
+    country_data.daily_confirmed_cases as new_cases,
+    country_data.daily_deaths as new_deaths,
+    country_data.confirmed_cases as cumulative_cases,
+    country_data.deaths as cumulative_deaths,
 from
-    {{ source('covid_sources', 'ecdc_daily_country_volume')}}
+    {{ source('covid_sources', 'ecdc_daily_country_volume')}} as country_data
+
+left join
+    {{ source('mapping', 'country_codes') }} as country_map
+on
+    country_map.country_code = country_data.country_territory_code
 
 {% if is_incremental() and max_partition_date is not none %}
 where
--- exploratory_work/country_code_population.sql
+++ exploratory_work/country_code_population.sql
@@ -1,21 +1,60 @@
 SELECT
   DISTINCT a.countries_and_territories AS source_country_value,
   b.country AS mapping_country,
+--   c.country_name AS mapping_country_name,
+--   a.geo_id AS source_geo_id,
+  a.country_territory_code AS source_country_territory_code,
   b.country_code AS mapping_country_code,
-  a.geo_id AS source_geo_id,
-  a.pop_data_2019,
-  b.year_2018,
-  a.pop_data_2019 - b.year_2018 AS pop_diff --we want pop data from ecdc (commonly eurostat it seems?)
+--   c.stanag_code AS mapping_stanag_code,
+--   c.fips_code AS mapping_fips_code
+--   a.pop_data_2019,
+--   b.year_2018,
+--   a.pop_data_2019 - b.year_2018 AS pop_diff --we want pop data from ecdc (commonly eurostat it seems?)
 FROM
   `bigquery-public-data.covid19_ecdc.covid_19_geographic_distribution_worldwide` AS a
+
 LEFT JOIN
   `bigquery-public-data.world_bank_global_population.population_by_country` AS b
 ON
   b.country_code = a.country_territory_code
--- WHERE
+
+-- This is out, way too many mismatches
+-- LEFT JOIN
+--   `bigquery-public-data.utility_us.country_code_iso` AS c
+-- ON
+--   c.fips_code = a.geo_id
+
+WHERE
 -- I can factor in for this
---   b.country IS NULL
+  b.country IS NULL
+-- c.country_name IS NULL
 ORDER BY
---   a.countries_and_territories ASC
+  a.countries_and_territories ASC
 --   pop_diff DESC
-  a.pop_data_2019 DESC
+--   a.pop_data_2019 DESC
+
+
+
+-- these aren't in mapping dataset with another code
+SELECT
+country, country_code
+FROM
+  `bigquery-public-data.world_bank_global_population.population_by_country`
+WHERE
+LOWER(country) LIKE '%anguilla%'
+
+
+-- And not getting any inflation. all good
+SELECT
+  DISTINCT countries_and_territories
+FROM
+  `bigquery-public-data.covid19_ecdc.covid_19_geographic_distribution_worldwide`
+
+
+-- Looks good
+SELECT
+  DISTINCT country_territory_name
+FROM
+  `big-query-horse-play.dbt_covid_dev_cleansed.ecdc_country_daily_volume`
+ORDER BY
+  country_territory_name DESC
