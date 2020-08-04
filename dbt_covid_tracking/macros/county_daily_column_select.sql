7c80a00f4e2e18f3fc2d9526fe0a55e6a6036f8c
unknown <moritzjordan@gmail.com>
2020-08-03 20:20:04 -0700

    Update to state_fips_code cleanse to be more resilient

--- dbt_covid_tracking/macros/county_daily_column_select.sql
+++ dbt_covid_tracking/macros/county_daily_column_select.sql
@@ -6,8 +6,8 @@ Naming and transformations are identical except metric.
 {%- macro county_daily_column_select() -%}
 
     -- Some states have malformed codes for whatever reason
-    if(length(state_fips_code) = 3
-      , substr(state_fips_code, 2)
+    if(length(state_fips_code) > 2
+      , substr(state_fips_code, -2, 2)
       , state_fips_code) as state_fips_code,
     state as state_abbreviation,
     -- These are technically geo_id values which are
