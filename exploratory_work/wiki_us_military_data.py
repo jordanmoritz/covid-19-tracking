01f9d8034cc6ee030fa6ad74fc041bb9e18f939b
unknown <moritzjordan@gmail.com>
2020-08-10 17:42:46 -0700

    Cleaned US war casualty data

--- exploratory_work/wiki_us_military_data.py
+++ exploratory_work/wiki_us_military_data.py
@@ -10,6 +10,23 @@ soup = BeautifulSoup(response.text, 'lxml')
 
 tables = soup.find_all('table')
 
-df = pd.read_html(str(table))
+df_list = pd.read_html(str(tables))
 
-# df[3] is what I'm looking for
+# df[1] is what I'm looking for
+
+us_war_df = df_list[1].iloc[:, [1, 3]].copy()
+
+us_war_df.describe()
+
+# us_war_casualties_df.drop(0, axis='index')
+
+us_war_df.dtypes
+
+# Fix malformed value for civil war
+us_war_df.iat[0, 1] = us_war_df.iat[0, 1][:7].replace(',', '')
+
+us_war_df.astype({'Deaths': 'int64'}).dtypes
+
+us_war_df.rename(columns={'War': 'war_name',
+                        'Deaths': 'total_deaths'},
+                inplace=True)
