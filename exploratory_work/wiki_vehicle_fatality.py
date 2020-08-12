2be00f653c36b0cd6d4342363bf5ee96e89e790e
unknown <moritzjordan@gmail.com>
2020-08-12 11:32:19 -0700

    Completed wiki vehicle data

--- exploratory_work/wiki_vehicle_fatality.py
+++ exploratory_work/wiki_vehicle_fatality.py
@@ -14,7 +14,23 @@ df_list = pd.read_html(str(tables))
 
 us_vehicle_df = df_list[0][['Year', 'Deaths']].copy()
 
+# Rename columns
+us_vehicle_df.rename(columns={'Year': 'year',
+                        'Deaths': 'total_deaths'},
+                inplace=True)
 
+us_vehicle_df = us_vehicle_df.apply(lambda x: x.str.strip(), axis='columns')
 
-print(cdc_deaths_df.dtypes)
-print(cdc_deaths_df)
+# Clean year formatting
+us_vehicle_df['year'] = us_vehicle_df['year'].apply(lambda x: x[:4])
+
+# Fix issue with death data
+us_vehicle_df.iat[0, 1] = us_vehicle_df.iat[0, 1][:2]
+
+# Cast data types
+us_vehicle_df = us_vehicle_df.astype({'total_deaths': 'int64'})
+
+print(us_vehicle_df.dtypes)
+print(us_vehicle_df)
+print(us_vehicle_df.total_deaths.unique())
+print(us_vehicle_df.year.unique())
